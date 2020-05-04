173423ed1844f2f4b85ff4ca4439d0045c964229
Eric <ew398@cornell.edu>
2020-05-04 11:02:33 -0400

    Create testcases.py

---
+++ testcases.py
@@ -0,0 +1,14 @@
+# Test cases with no solution
+# Violating Constraint 1:
+
+# Violating Constraint 2:
+
+# Violating Constraint 3:\
+
+# Violating Constraint 4:
+
+# Violating Constraint 5:
+
+# Test cases with unique solutions
+
+# Test cases with multiple solutions
